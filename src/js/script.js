51e4e06a794fbf99630f51fbe32a45457c399a2c
Vitalii Vovk <99590889+vovkvi@users.noreply.github.com>
2022-12-15 16:44:29 +0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,21 @@
 var pg = getUrlParam('page', undefined);
-Hyphenopoly.config({
-    require: {
-        "ru": "восьмидесятивосьмимиллиметровое",
-        "en-us": "Supercalifragilisticexpialidocious"
-    },
-    paths: {
-        patterndir: "./src/libs/hyphenopoly/patterns/",
-        maindir: "./src/libs/hyphenopoly/"
-    },
-    setup: {
-        selectors: {
-            ".content": {}
-        }
-    }
-});
+
 $(document).ready(() => {
+    Hyphenopoly.config({
+        require: {
+            "ru": "восьмидесятивосьмимиллиметровое",
+            "en-us": "Supercalifragilisticexpialidocious"
+        },
+        paths: {
+            patterndir: "./src/libs/hyphenopoly/patterns/",
+            maindir: "./src/libs/hyphenopoly/"
+        },
+        setup: {
+            selectors: {
+                ".content": {}
+            }
+        }
+    });
     $('.footer').html('&copy; Vitalii Vovk, ' + new Date().getFullYear());
     $.getScript('./src/js/config.js', () => {
         if (pg != undefined) {
